cfd5d58ca9d326163876263c44cb67efbcb67167
Hajime Shiozawa <shiozawa@ripplex.com>
2017-02-16 15:39:37 +0900

    using MyBottomNavigationView

--- app/src/main/java/com/hjm/bottomnavigationviewsample/MainFragment.java
+++ app/src/main/java/com/hjm/bottomnavigationviewsample/MainFragment.java
@@ -2,13 +2,14 @@ package com.hjm.bottomnavigationviewsample;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.hjm.bottomnavigationviewsample.bottomnav.MyBottomNavigationView;
 
 public class MainFragment extends Fragment {
 
@@ -18,8 +19,8 @@ public class MainFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         final TextView textView = (TextView) view.findViewById(R.id.text);
 
-        BottomNavigationView navView = (BottomNavigationView) view.findViewById(R.id.bottomNav);
-        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        MyBottomNavigationView navView = (MyBottomNavigationView) view.findViewById(R.id.bottomNav);
+        navView.setOnNavigationItemSelectedListener(new MyBottomNavigationView.MyOnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int itemId = item.getItemId();
